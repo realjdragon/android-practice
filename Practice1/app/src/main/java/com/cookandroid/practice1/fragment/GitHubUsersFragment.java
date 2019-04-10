6217458fbd382e35700068eccd45ed75d9a5c299
신재용 <jaesin@ebay.com>
2019-04-10 12:52:30 +0900

    무한 스크롤 돌아만 간다

--- Practice1/app/src/main/java/com/cookandroid/practice1/fragment/GitHubUsersFragment.java
+++ Practice1/app/src/main/java/com/cookandroid/practice1/fragment/GitHubUsersFragment.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.cookandroid.practice1.R;
+import com.cookandroid.practice1.adapter.EndlessScrollListener;
 import com.cookandroid.practice1.adapter.ItemDealAdapter;
 import com.cookandroid.practice1.api.GitHubApiClient;
 import com.cookandroid.practice1.entity.ItemDeal;
@@ -58,6 +59,18 @@ public class GitHubUsersFragment extends Fragment {
         // AdapterView라고 부르는 이유는 UI에 표시할 항목을 adapter라는 객체에서 공급받기 때문
         itemListView = (ListView)rootView.findViewById(R.id.item_deal_list);
 
+        // Attach the listener to the AdapterView onCreate
+        itemListView.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to your AdapterView
+                setMobileHome(page);
+                // or loadNextDataFromApi(totalItemsCount);
+                return true; // ONLY if more data is actually being loaded; false otherwise.
+            }
+        });
+
         // 위에 만든 Adapter 객체를 ListView에 설정.
         itemListView.setAdapter(adapter);
 
@@ -65,19 +78,19 @@ public class GitHubUsersFragment extends Fragment {
         setMobileHomeSwipeRefreshLayout();
 
         // API 호출해서 ListView Set
-        setMobileHome();
+        setMobileHome(1);
 
         return rootView;
     }
 
-    private void setMobileHome() {
+    private void setMobileHome(int page) {
         // 안드로이드는 안정성의 이유로 Main Thread (UI Thread)에서만 UI를 변경할 수 있도록 제한됨
         // 또, Main Thread에서 네트워크작업도 제한됨..5초 이상의 지연이 있을 경우 App 종료..
         // 따라서 별도의 Thread를 구성하고 네트워크 작업을 해야하는데 매번 이러긴 귀찮으니
         // Volley라는 패키지를 사용해서 손 쉽게 API를 호출하고 response 후에 UI 작업까지 할 수 있음.
         new GitHubApiClient().searchUsers(
                 "to",
-                1,
+                page,
                 new Response.Listener<SearchUsersApiResponse>() {
                     @Override
                     public void onResponse(SearchUsersApiResponse response) {
@@ -108,8 +121,6 @@ public class GitHubUsersFragment extends Fragment {
     }
 
     private void processMobileHomeResponse(SearchUsersApiResponse response) {
-        deals.clear();
-
         ArrayList<SearchUsersApiResponse.User> users = response.getUsers();
 
         if (users != null && users.size() > 0) {
@@ -126,7 +137,7 @@ public class GitHubUsersFragment extends Fragment {
         githubSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                setMobileHome();
+                setMobileHome(1);
             }
         });
     }
