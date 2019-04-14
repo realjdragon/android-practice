bd8f94b1337fa59e44e7498e0c4a846440d3b094
신재용 <jaesin@ebay.com>
2019-04-15 08:38:25 +0900

    fix

--- Practice1/app/src/main/java/com/cookandroid/practice1/api/HomeMainApiClient.java
+++ Practice1/app/src/main/java/com/cookandroid/practice1/api/HomeMainApiClient.java
@@ -2,19 +2,22 @@ package com.cookandroid.practice1.api;
 
 import com.android.volley.Response;
 import com.cookandroid.practice1.MyApplication;
-import com.cookandroid.practice1.entity.home.HomeMainApiResponse;
+import com.cookandroid.practice1.entity.api.home.HomeMainApiResponse;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HomeMainApiClient {
+    private String urlPrefix = "";
 
     public void getMobileHome(Response.Listener<HomeMainApiResponse> responseListener, Response.ErrorListener errorListener)
     {
         String url = urlPrefix
+                + "";;
 
         Map<String, String> params = new HashMap<String, String>();
         params.put("Content-Type", "application/json");
+        params.put("Authorization", "");
 
         // Volley는 onResponse, onErrorResponse 리스너를 정의하는걸 강제하고 있음.
         GsonRequest<HomeMainApiResponse> request = new GsonRequest<HomeMainApiResponse>(
