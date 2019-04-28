06e522b30468785e30ffc320d6e7542d7973ddfd
jdragon <jaeyongjaeyongjaeyong@gmail.com>
2019-04-28 17:12:41 +0900

    워후

--- Practice1/app/src/main/java/com/cookandroid/practice1/api/HomeMainApiClient.java
+++ Practice1/app/src/main/java/com/cookandroid/practice1/api/HomeMainApiClient.java
@@ -8,16 +8,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HomeMainApiClient {
-    private String urlPrefix = "http://gmapi.gmarket.co.kr";
+    private String urlPrefix = "";
 
     public void getMobileHome(Response.Listener<HomeMainApiResponse> responseListener, Response.ErrorListener errorListener)
     {
         String url = urlPrefix
-                + "/api/Home/GetMobileHomeMainV2";;
+                + "";;
 
         Map<String, String> params = new HashMap<String, String>();
         params.put("Content-Type", "application/json");
-        params.put("Authorization", "Basic IQB0AE4AdQArAHEAcwBWAHkAWgBXAFoAbABQAHQAKwBXAGUANwBaAHYAawBjAE4AeAArAG8AUAA2AHUAYwByACsAQwBrAEsANQBFADQAOABxAFEANgBlAGcASQB2ACsAagBRAGIATgBKAGEAMwAyAE8AUwBqAFcANQBJADYAawB2AG4ATAA4ACsASABaAGoAYQBkAE4AZAArAFUAVQBpAFYATABzAEkAMwBtAFgAbwBuAFQATwBRAGYAMwBjAHkAWABuAEkAMgBWAGUARABaAGIAWABHAE4AVgBTAGwAMABxAEcALwBoAGgAUgBGAFgAWABjAHIARgBaAHkANwBiADUAWABpAEgASgBMACsAVABNADcATgBXAGQAdgAvAG4AcgBCADIAMwBDAFYAZwA9AD0A");
+        params.put("Authorization", "");
 
         // Volley는 onResponse, onErrorResponse 리스너를 정의하는걸 강제하고 있음.
         GsonRequest<HomeMainApiResponse> request = new GsonRequest<>(
